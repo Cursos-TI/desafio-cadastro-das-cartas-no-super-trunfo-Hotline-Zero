8b24ff85d2bdb145a22e14a5e2255260416bc64c
Hotline-Zero <victorgabriel0205200@gmail.com>
2025-03-24 13:02:16 +0000

    Estrutura de variaveis pronta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,17 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    // Definição da estrutura 'Carta' que representa uma carta com várias informações
+typedef struct {
+    char estado;                // Estado representado pela carta (A-H)
+    char codigo[4];             // Código da carta (ex: A01)
+    char nomeCidade[50];        // Nome da cidade
+    int populacao;              // População da cidade
+    float area;                 // Área da cidade em km²
+    float pib;                  // PIB da cidade em bilhões de reais
+    int pontosTuristicos;       // Número de pontos turísticos na cidade
+} Carta;
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
